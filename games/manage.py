120d4e648b3f997375bb38b231076837179736fe
sharanms88 <sharansrinivasan92@gmail.com>
2020-03-23 01:38:39 -0700

    redo

--- games/manage.py
+++ games/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.games.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'games.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
